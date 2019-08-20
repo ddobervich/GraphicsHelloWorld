27bb8a31b7aeaef33700b6eb07585711d10396ec
David Dobervich <david.dobervich+apcsgithub@gmail.com>
2019-08-19 18:17:07 -0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public void settings() {
+        size(600, 600);
+    }
+
+    public void setup() {
+    }
+
+    public void draw() {
+
+        // Draw circle in the middle of the window
+        ellipse(width/2, height/2, 20, 20);
+
+        // -= YOUR CODE HERE =-
+
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+}
